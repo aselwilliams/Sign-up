d34356f26270f8302056f800ce30efb44b10a298
aselwilliams <aselwilliams22@gmail.com>
2022-06-08 13:20:49 -0400

    all components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import {BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from './components/Dashboard';
+import LoginPage from './components/LoginPage';
+import NotFound from './components/NotFound';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Router>
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path ='/sign-up' element={<SignUp />} />
+              <Route path ='*' element={<NotFound />} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
       </div>
   );
 }
